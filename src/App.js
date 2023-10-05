70df8f5e3deaa3edb8d24c999918d090cab8ea7c
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-10-06 01:43:17 +0530

    your message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Corporate } from './component/corporate/corporate';
 import { PropertyFooterWrapper } from './component/footer/footer';
 
 function App() {
   return (
-    <footer>
+    <>
+    <div style={{display:"flex" , justifyContent:"center"}}> <Corporate/></div>
+  
+     <footer>
       <PropertyFooterWrapper/>
     </footer>
+    </>
+   
   );
 }
 
---
+++ src/component/group/group.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./group.css";
+export const Box = () => {
+return (
+<div className="box">
+<div className="rectangle" />
+</div>
+);
+}; 
